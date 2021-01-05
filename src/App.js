51f54bbff963db82c6f7cfbf35d4e96efb890748
Lemon <516330797@qq.com>
2021-01-05 10:42:57 +0800

    替换 PurComponent

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-class App extends React.Component {
+class App extends React.PureComponent {
   constructor(props) {
     super(props)
     this.state = {
@@ -11,10 +11,6 @@ class App extends React.Component {
   onClick = () => {
     this.setState(state => ({n: state.n + 1}))
     this.setState(state => ({n: state.n - 1}))
-  }
-
-  shouldComponentUpdate(newProps, newState) {
-    return newState.n !== this.state.n
   }
 
   render() {
