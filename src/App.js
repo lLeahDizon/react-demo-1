312bdeaa32b5e0cbc5f3af20c5024d327d9e0955
Lemon <516330797@qq.com>
2021-01-06 15:41:55 +0800

    函数组件实践

--- src/App.js
+++ src/App.js
@@ -1,33 +1,15 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-class App extends React.PureComponent {
-  divRef = undefined
-
-  constructor(props) {
-    super(props)
-    this.state = {
-      n: 1,
-      width: undefined
-    }
-    this.divRef = React.createRef()
+const App = props => {
+  const [n, setN] = useState(0)
+  const onClick = () => {
+    setN(n + 1)
   }
-
-  onClick = () => {
-    this.setState(state => ({n: state.n + 1}))
-  }
-
-  componentDidMount() {
-    // const div = document.getElementById('xxx')
-    const div = this.divRef.current
-    const {width} = div.getBoundingClientRect()
-    this.setState({width})
-  }
-
-  render() {
-    return (
-      <div ref={this.divRef} id="xxx">Hello World, {this.state.width}</div>
-    )
-  }
+  return (
+    <div>{n}
+      <button onClick={onClick}>+1</button>
+    </div>
+  )
 }
 
 export default App
