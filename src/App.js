d97aae18ee69ee6fc49c43c553503c97adc728f3
Lemon <516330797@qq.com>
2021-01-06 16:13:05 +0800

    模拟 componentWillUnMount

--- src/App.js
+++ src/App.js
@@ -1,34 +1,32 @@
 import React, {useState, useEffect} from 'react'
 
 const App = props => {
-  const [n, setN] = useState(0)
-  const [m, setM] = useState(0)
-  const onClickN = () => {
-    setN(n + 1)
+  const [childVisible, setChildVisible] = useState(true)
+  const show = () => {
+    setChildVisible(!childVisible)
   }
-
-  const onClickM = () => {
-    setM(m + 1)
-  }
-
-  // 模拟 componentDidMount
-  useEffect(() => {
-    console.log('use effect')
-  }, [])
-
-  // 模拟 componentDidUpdate
-  useEffect(() => {
-    console.log('state 变了')
-  })
 
   return (
     <div>
-      {n}
-      <button onClick={onClickN}>+1</button>
-      <hr/>
-      {m}
-      <button onClick={onClickM}>+1</button>
+      <button onClick={show}>{childVisible ? 'hide' : 'show'}</button>
+      {childVisible ? <Child/> : null}
     </div>
+  )
+}
+
+const Child = (props) => {
+  // 模拟 componentWillUnMount
+  useEffect(() => {
+    // 函数本身是在渲染的时候执行的
+    // 函数的返回是在销毁的时候执行的
+    console.log('渲染或者变化')
+    return () => {
+      console.log('Child 销毁了')
+    }
+  })
+
+  return (
+    <div>Child</div>
   )
 }
 
