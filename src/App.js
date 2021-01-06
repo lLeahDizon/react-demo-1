36aba98c64f5d7267984e2116f1bd812aba7be76
Lemon <516330797@qq.com>
2021-01-06 16:00:15 +0800

    useEffect 实践

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 
 const App = props => {
   const [n, setN] = useState(0)
-  const onClick = () => {
+  const [m, setM] = useState(0)
+  const onClickN = () => {
     setN(n + 1)
   }
+
+  const onClickM = () => {
+    setM(m + 1)
+  }
+
+  // 模拟 componentDidMount
+  useEffect(() => {
+    console.log('use effect')
+  }, [])
+
+  // 模拟 componentDidUpdate
+  useEffect(() => {
+    console.log('state 变了')
+  })
+
   return (
-    <div>{n}
-      <button onClick={onClick}>+1</button>
+    <div>
+      {n}
+      <button onClick={onClickN}>+1</button>
+      <hr/>
+      {m}
+      <button onClick={onClickM}>+1</button>
     </div>
   )
 }
