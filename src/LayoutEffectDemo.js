89996dc524ffd6554734b3d5bb4285e6371da7c6
Lemon <516330797@qq.com>
2021-01-14 23:16:19 +0800

    比较 useLayoutEffect 和 useEffect 执行时间

--- src/LayoutEffectDemo.js
+++ src/LayoutEffectDemo.js
@@ -1,15 +1,25 @@
-import React, {useState, useLayoutEffect} from 'react'
+import React, {useState, useLayoutEffect, useEffect, useRef} from 'react'
 
 function App() {
   const [value, setValue] = useState(0)
+  const time = useRef(null)
+  const onClick = () => {
+    setValue(i => i + 1)
+    time.current = performance.now() // performance 是一个全局变量，可以打印出当前时间
+  }
 
   // 插入到 DOM 和 render 之间执行，不会出现闪烁
-  useLayoutEffect(() => {
-    document.querySelector('#x').innerText = 'value: 1000'
-  }, [value])
+  useEffect(() => {
+    if (time.current) {
+      console.log(performance.now() - time.current)
+    }
+  })
 
   return (
-    <div id='x' onClick={() => setValue(0)}>value: {value}</div>
+    <div className='App'>
+      <h1>value: {value}</h1>
+      <button onClick={onClick}>Click</button>
+    </div>
   )
 }
 
