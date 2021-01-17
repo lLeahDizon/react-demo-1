f00d3557488e561afdd177fbc7f74d44b8495270
Lemon <516330797@qq.com>
2021-01-17 21:54:14 +0800

    封装自定义 Hooks

--- src/CustomizeDemo.js
+++ src/CustomizeDemo.js
@@ -2,14 +2,17 @@ import React, {useRef, useState, useEffect} from 'react'
 import useList from './hooks/useList'
 
 function App() {
-  const {list} = useList()
+  const {list, deleteIndex, addItem} = useList()
   return (
     <div className="App">
       <h1>List</h1>
       {list ? (
         <ol>
-          {list.map(item => (
-            <li key={item.id}>{item.name}</li>
+          {list.map((item, index) => (
+            <li key={item.id}>
+              {item.name}
+              <button onClick={() => {deleteIndex(index)}}>x</button>
+            </li>
           ))}
         </ol>
       ) : (
--- src/hooks/useList.js
+++ src/hooks/useList.js
@@ -9,7 +9,12 @@ const useList = () => {
   }, [])
   return {
     list: list,
-    setList: setList
+    addItem: name => {
+      setList([...list, {id: Math.random(), name}])
+    },
+    deleteIndex: index => {
+      setList(list.slice(0, index).concat(list.slice(index + 1)))
+    }
   }
 }
 
